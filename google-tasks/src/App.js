0ddd26d67df46d0f6d03e9bcfd98fff3892540f7
Ariana <ariana.berger@gmail.com>
2019-10-09 11:44:34 -0400

    Add main task area and basic styling

---
+++ google-tasks/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-main">
+          <h2>My Tasks</h2>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
